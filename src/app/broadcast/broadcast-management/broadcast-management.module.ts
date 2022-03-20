50032d665088220cc13a473fb3b92285413981d9
Shreya Verma <shreya.verma@plantix.net>
2022-03-20 19:03:02 +0530

    showing how to work with git

--- src/app/broadcast/broadcast-management/broadcast-management.module.ts
+++ src/app/broadcast/broadcast-management/broadcast-management.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { BroadcastManagementRoutingModule } from './broadcast-management-routing.module';
 import { BroadcastComponent1Component } from './broadcast-component1/broadcast-component1.component';
 import { BroadcastComponent2Component } from './broadcast-component2/broadcast-component2.component';
+import { TestComponent } from './test/test.component';
 
 
 @NgModule({
   declarations: [
     BroadcastComponent1Component,
-    BroadcastComponent2Component
+    BroadcastComponent2Component,
+    TestComponent
   ],
   imports: [
     CommonModule,
